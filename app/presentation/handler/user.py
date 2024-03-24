73edd7f6235f1539c5981a12bf79e901c8da33bb
RyotArch <r.yamamoto.system@gmail.com>
2024-03-24 12:59:55 +0900

    feat

--- app/presentation/handler/user.py
+++ app/presentation/handler/user.py
@@ -1,10 +1,11 @@
+from typing import Dict
 from interface.usecase.user import UserUsecaseInterface
 
 class UserHandler:
     def __init__(self, usecase: UserUsecaseInterface) -> None:
         self.uc = usecase
 
-    def hello_world(self) -> object:
+    def hello_world(self) -> Dict[str, str]:
        return {
            "say": self.uc.hello_world()
         }
